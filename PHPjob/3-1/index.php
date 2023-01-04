b5cd02831aa03648bbf020c8809efa548e2b3916
daikun0919 <daikin091964@gmail.com>
2023-01-04 12:07:29 +0900

    FuzzBuzz問題
    FuzzBuzz問題の実装

---
+++ PHPjob/3-1/index.php
@@ -0,0 +1,17 @@
+<?php
+  for ($num = 1 ; $num <= 100 ; $num++) {
+    if ($num % 3 === 0 && $num % 5 === 0) {
+      echo "FizzBuzz!!";
+      echo "<br>";
+    } elseif ($num % 3 === 0){
+      echo "Fuzz!";
+      echo "<br>";
+    } elseif ($num % 5 === 0) {
+      echo "Buzz!";
+      echo "<br>";
+    } else {
+      echo $num;
+      echo "<br>";
+    }
+  }
+?>
